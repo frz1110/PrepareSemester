e279da210e5d94d53b665eaaa150ca65374cf3fe
Farzana Hadifa <hadifafarzana@gmail.com>
2022-05-17 18:10:59 +0700

    ubah jadwal

--- jadwalmatkul.pl
+++ jadwalmatkul.pl
@@ -2,12 +2,36 @@ jumlah_sks(law, 3).
 jumlah_sks(prolog, 4).
 jumlah_sks(datmin, 3).
 jumlah_sks(persdif, 3).
+jumlah_sks(ddp2, 4).
+jumlah_sks(matdis2, 3).
+jumlah_sks(ppl, 6).
+jumlah_sks(tba, 4).
+jumlah_sks(basdat, 4).
+jumlah_sks(komas, 3).
+jumlah_sks(kriptografi, 4).
+jumlah_sks(adpro, 4).
 
-waktu(rabu, law, 09:00, 09:50).
-waktu(kamis, law, 09:00, 10:40). 
-waktu(senin, prolog, 08:00, 10:40). 
-waktu(selasa, prolog, 08:00, 09:40). 
-waktu(selasa, datmin, 13:00, 14:40). 
-waktu(jumat, datmin, 09:00, 10:50). 
-waktu(selasa, persdif, 13:00, 14:40). 
-waktu(sabtu, persdif, 09:00, 10:50).
+waktu(rabu, law, 08:00, 08:50).
+waktu(jumat, law, 08:00, 09:40).
+waktu(senin, prolog, 13:00, 14:40).
+waktu(kamis, prolog, 08:00, 09:40).
+waktu(selasa, datmin, 13:00, 14:40).
+waktu(kamis, datmin, 13:00, 13:50).
+waktu(rabu, persdif, 13:00, 14:40).
+waktu(jumat, persdif, 10:00, 10:50).
+waktu(senin, ddp2, 13:00, 14:40).
+waktu(rabu, ddp2, 13:00, 14:40).
+waktu(kamis, ddp2, 15:00, 16:40).
+waktu(senin, matdis2, 15:00, 16:40).
+waktu(rabu, matdis2, 11:00, 11:50).
+waktu(selasa, ppl, 09:00, 11:30).
+waktu(rabu, ppl, 09:00, 11:30).
+waktu(senin, tba, 15:00, 16:40).
+waktu(rabu, tba, 14:00, 15:40).
+waktu(kamis, basdat, 15:00, 16:40).
+waktu(jumat, basdat, 10:00, 11:40).
+waktu(selasa, komas, 09:00, 11:30).
+waktu(selasa, kriptografi, 15:00, 16:40).
+waktu(kamis, kriptografi, 15:00, 16:40).
+waktu(selasa, adpro, 13:00, 14:40).
+waktu(senin, adpro, 13:00, 14:40).
